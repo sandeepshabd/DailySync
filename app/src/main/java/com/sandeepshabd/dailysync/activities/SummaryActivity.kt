08d7282e31904c21d45d3c2ad7f5befe823e3d60
Sandeep Shabd <sandeep.shabd@gm.com>
2018-01-04 05:38:55 -0500

    added dummy data to display

--- app/src/main/java/com/sandeepshabd/dailysync/activities/SummaryActivity.kt
+++ app/src/main/java/com/sandeepshabd/dailysync/activities/SummaryActivity.kt
@@ -2,12 +2,44 @@ package com.sandeepshabd.dailysync.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.sandeepshabd.dailysync.R
+import org.jetbrains.anko.find
 
 class SummaryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+        var pieChart = find<PieChart>(R.id.pieChart)
+
+       var entryForChart = mutableListOf<PieEntry>()
+        entryForChart.add(PieEntry(4f, 0f))
+        entryForChart.add(PieEntry(8f, 0f))
+        entryForChart.add(PieEntry(6f, 0f))
+        entryForChart.add(PieEntry(9f, 0f))
+        entryForChart.add(PieEntry(15f, 0f))
+
+        var dataSet = PieDataSet(entryForChart,getResources().getString(R.string.numbers))
+        dataSet.colors = ColorTemplate.COLORFUL_COLORS.toMutableList()
+
+        var labelList = ArrayList<String>()
+        labelList.add("Mon")
+        labelList.add("Tue")
+        labelList.add("Wed")
+        labelList.add("Thu")
+        labelList.add("Fri")
+
+        var pieData = PieData(dataSet)
+        pieChart.data = pieData
+
+
+
+
     }
 }

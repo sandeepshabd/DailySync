3ee1356806f22c41d808471586dd11dde9e94793
Sandeep Shabd <sandeep.shabd@gm.com>
2018-01-04 05:57:36 -0500

    adding percentage for the week

--- app/src/main/java/com/sandeepshabd/dailysync/activities/SummaryActivity.kt
+++ app/src/main/java/com/sandeepshabd/dailysync/activities/SummaryActivity.kt
@@ -1,8 +1,10 @@
 package com.sandeepshabd.dailysync.activities
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
@@ -19,24 +21,27 @@ class SummaryActivity : AppCompatActivity() {
         var pieChart = find<PieChart>(R.id.pieChart)
 
        var entryForChart = mutableListOf<PieEntry>()
-        entryForChart.add(PieEntry(4f, 0f))
-        entryForChart.add(PieEntry(8f, 0f))
-        entryForChart.add(PieEntry(6f, 0f))
-        entryForChart.add(PieEntry(9f, 0f))
-        entryForChart.add(PieEntry(15f, 0f))
+        entryForChart.add(PieEntry(4f, "Mon"))
+        entryForChart.add(PieEntry(8f, "Tue"))
+        entryForChart.add(PieEntry(6f, "Wed"))
+        entryForChart.add(PieEntry(9f, "Thu"))
+        entryForChart.add(PieEntry(15f, "Fri"))
 
         var dataSet = PieDataSet(entryForChart,getResources().getString(R.string.numbers))
         dataSet.colors = ColorTemplate.COLORFUL_COLORS.toMutableList()
-
-        var labelList = ArrayList<String>()
-        labelList.add("Mon")
-        labelList.add("Tue")
-        labelList.add("Wed")
-        labelList.add("Thu")
-        labelList.add("Fri")
-
+        
         var pieData = PieData(dataSet)
         pieChart.data = pieData
+        pieChart.data.setValueTextSize(15f)
+        pieChart.data.setValueTextColor(Color.DKGRAY)
+
+
+        var description =  Description()
+        description.text = "Pie chart for Daily sync"
+        pieChart.setDescription(description)
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setTransparentCircleRadius(30f);
+        pieChart.setHoleRadius(30f);
 
 
 
